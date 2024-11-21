4d0437bc1bd9fe942b612c85fdea1e4eeb410014
sipandrei <pontos.serban@gmail.com>
2024-11-21 23:15:18 +0200

    fixed debounce and tested sd functionality

--- openLoadCell/openLoadCell.ino
+++ openLoadCell/openLoadCell.ino
@@ -12,57 +12,66 @@ const int clockPinBridge = 6;
 
 const int SDPin = 10;
 const int buttonPin = 4;
+Sd2Card card;
 
-int startState = LOW;
-int debounce = 3;
+int buttonState = 1;
+int debounce = 50;
 int lastDebounce = 0;
+bool isRecording = false;
 
 void setup() {
-  Serial.begin(11500);
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(3,0);
+  Serial.begin(115200);
+  //lcd.init();
+  //lcd.backlight();
+  //lcd.setCursor(3,0);
   
 
-  bridge.begin(dataPinBridge, clockPinBridge);
+  //bridge.begin(dataPinBridge, clockPinBridge);
 
   pinMode(buttonPin, INPUT_PULLUP);
   
   if(!SD.begin(SDPin)){
     Serial.println("SD initialization failed");
-    lcd.clear();
-    lcd.print("SD initialization FAILED!");
+    //lcd.clear();
+    //lcd.print("SD initialization FAILED!");
     while(1);
   }
   Serial.println("SD initialized");
-  lcd.print("SD initialized!");
+  //lcd.print("SD initialized!");
   
-  lcd.clear();
-  lcd.print("Waiting for start command");
-  while(startState = LOW){
-    buttonDebounce(digitalRead(buttonPin), startState);
+  //lcd.clear();
+  //lcd.print("Waiting for start command");
+  while(isRecording == false){
+    buttonDebounce(digitalRead(buttonPin));
+    
+    //Serial.println(isRecording);
   }
 
-  lcd.clear();
-  lcd.print("Initializing test...");
-
-  deformationRecording.open(nameMaker(), FILE_WRITE);
+  //lcd.clear();
+  //lcd.print("Initializing test...");
+  Serial.print("Initializing test...");
+  deformationRecording= SD.open(nameMaker(), FILE_WRITE);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  //buttonDebounce(digitalRead(buttonPin));
 }
 
-void buttonDebounce(int buttonReading, int &oldState){
-  if(buttonReading != oldState)
+void buttonDebounce(int buttonReading){ Serial.print(buttonState);
+  if(buttonReading != buttonState and lastDebounce == 0)  
     lastDebounce = millis();
-  if(millis()-lastDebounce > debounce){
-    oldState = buttonReading;
+  if((millis()-lastDebounce) > debounce){
+    if(buttonReading == 1 and buttonState == 0)
+      isRecording = !isRecording;
+    if(buttonReading != buttonState)
+      buttonState = buttonReading;
+      lastDebounce = 0;
     }
 }
 
-int nameMaker(){
+String nameMaker(){
   String filename;
   int testNumber = 0;
   String prefix = "test";
