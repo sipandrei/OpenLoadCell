07eec1886046a5dc389915a084caf55a24e89569
sipandrei <pontos.serban@gmail.com>
2025-03-02 13:51:09 +0200

    added reading, display and parsing of data, PROBLEM with saving

--- openLoadCell/openLoadCell.ino
+++ openLoadCell/openLoadCell.ino
@@ -1,5 +1,7 @@
 // The format of a line of the config file SHALL be: <nominal resistance>,<compensation factor>
 // First line of config.txt SHALL be the header
+// First data line SHALL contain information about the poisson ratio, εs, calibration factor and bridge setup,
+// after the resistance and compensation factor, IN THIS ORDER!!!
 
 #include <LiquidCrystal_I2C.h>
 #include <HX711.h>
@@ -11,11 +13,12 @@ File deformationRecording, configFile;
 LiquidCrystal_I2C lcd(0x27,20,4);
 HX711 bridge;
 
+int bridgeSetup = 1;
 float baseVoltage = 5.0;
-float wheatstoneVoltage = 0;
+float poisson = 0;
+float statDef = 0;
 float comp[4];
-float res[4];
-
+//float res[4];
 const int dataPinBridge = 5;
 const int clockPinBridge = 6;
 int calibrationValue = 0;
@@ -24,7 +27,7 @@ const int SDPin = 10;
 const int buttonPin = 4;
 Sd2Card card;
 long long startTime=0;
-CSV_Parser cp("ff");
+CSV_Parser cp("fffdd");
 
 char feedRowParser() {
   return configFile.read();
@@ -67,26 +70,34 @@ void setup() {
   }
   int i = 0;
 
-  float *rez = (float*)cp[0];
-  float *comps = (float*)cp[1];
+  float *comps = (float*)cp[0];
+  float *filePoisson = (float*)cp[1];
+  float *fileStatDef = (float*)cp[2];
+  int *fileCalFact = (int*)cp[3];
+  int *configSetup = (int*)cp[4];
   
   while(cp.parseRow()){
+    if(i == 0){
+      poisson = filePoisson[0];
+      statDef = fileStatDef[0];
+      calibrationValue = fileCalFact[0];
+      bridgeSetup = configSetup[0];
+      
+      Serial.println((String)poisson+" "+(String)statDef+" "+(String)calibrationValue+" "+(String)bridgeSetup);
+    }
+    
     if(i < 4){
-      res[i] = rez[0];
       comp[i] = comps[0];
-      Serial.print(res[i]);
-      Serial.print(" ");
       Serial.println(comp[i]);
       i++;
     } 
   }
   for(int j = 0; j < 4; j++){
-    if(res[j] == 0)
+    if(comp[j] == 0)
       {
-        Serial.println(res[j]);
         lcd.clear();
         lcd.print("Config Error");
-        lcd.print(" Wrong Res");
+        lcd.print(" ZERO Res or Comp");
         while(1);
       }
   }
@@ -99,21 +110,26 @@ void loop() {
       deformationRecording.close();
     lcd.clear();
     lcd.print("Waiting for start   command...");
+    
     while(isRecording == false){
       buttonDebounce(digitalRead(buttonPin));
     }
-
     lcd.clear();
     lcd.print("Initializing test...");
-    Serial.print("Initializing test...");
     deformationRecording= SD.open(nameMaker(), FILE_WRITE);
-    addCsvLine(deformationRecording, "Current Deformation", "Read Time");
+    addCsvLine(deformationRecording, "Raw Reading", "Read Time", "Voltage Reading", "Deformation Value");
     bridge.power_up();
     startTime = millis();
+    bridge.tare();
   }
-  int currentDeformation;
-  addCsvLine(deformationRecording,String(currentDeformation), String(int(millis()-startTime)));
+  
+  int rawReading = bridge.read();
+  float voltageReading = rawReading * baseVoltage / 16777216;
+  float deformationReading = deformationParsing(voltageReading);
+  addCsvLine(deformationRecording,String(rawReading), String(int(millis()-startTime)), String(voltageReading), String(deformationReading));
+  recordingDisplay((String)rawReading, (String)voltageReading, (String)deformationReading);
   buttonDebounce(digitalRead(buttonPin)); //verify test stop
+  delay(10);
 }
 
 void buttonDebounce(int buttonReading){ 
@@ -141,8 +157,54 @@ String nameMaker(){
   return filename;
 }
 
-void addCsvLine(File file, String deformation, String readTime){
+void addCsvLine(File file, String rawValue, String readTime, String readVoltage, String deformation){
   file.print(readTime);
+  file.print(",");
+  file.print(rawValue);
+  file.print(",");
+  file.print(readVoltage);
   file.print(",");
   file.println(deformation);
   }
+
+float deformationParsing(float voltage){
+  switch(bridgeSetup){
+    case 1:
+      return 4 * voltage / comp[0] / baseVoltage - statDef;
+    break;
+    
+    case 2:
+      return 4 * voltage / comp[0] / baseVoltage;
+    break;
+
+    case 3:
+      return 4 * voltage / (1 + poisson) / comp[0] / baseVoltage;
+    break;
+    
+    case 4:
+      return 2 * voltage / comp[0] / baseVoltage;
+    break;
+        
+    case 5:
+      return 2 * voltage / comp[0] / baseVoltage - statDef;
+    break;
+        
+    case 6:
+      return 2 * voltage / (1 + poisson) / comp[0] / baseVoltage; 
+    break;
+        
+    case 7:
+      return voltage / comp[0] / baseVoltage;
+    break;
+        
+    case 8:
+      return 2 * voltage / (1 - poisson) / comp[0] / baseVoltage;
+    break;
+  
+  }
+}
+
+void recordingDisplay(String r, String v, String e){
+  lcd.clear();
+  lcd.print("r:" + r + " v:" + v + " e:" + e);
+}
